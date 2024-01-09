bd9d29235eea236f8a0a7d80a9343d456904b4bc
Humza Qureshi <humzaqureshis@gmail.com>
2024-01-08 19:43:58 -0600

    asd

--- bin/ops.hpp
+++ bin/ops.hpp
@@ -119,39 +119,3 @@ static inline void print(T t, Args... args) {
 }
 
 #define println(...) print(__VA_ARGS__);printf("\n")
-
-/*template <typename T, typename... Args>
-static inline auto __index_wrapper__(T* t, Args... args) -> decltype(*__index__(t,args...)) {
-    return *__index__(t,args...);
-}*/
-
-
-/*template<typename T>
-struct _ndarray{
-    T* raw;
-    int* dims;
-    int ndims;
-};
-
-template <typename... Args>
-static inline int _get_index(int* dims, int ndims, int out, int i, Args... args) {
-    if (ndims == 1){
-        return out + i;
-    }
-    int this_idx = i;
-    for (int i = 1; i < ndims; i++){
-        this_idx *= dims[i];
-    }
-    return _get_index(dims,ndims-1,out + this_idx, args...);
-}
-
-template<typename T, typename... Args>
-static inline T* __index__(struct _ndarray<T>* arr, Args... args){
-    int idx = _get_index(arr->dims,arr->dims,0,args...);
-    
-}
-
-typedef struct _ndarray<double>* _DoubleArray;
-typedef struct _ndarray<float>* _FloatArray;
-typedef struct _ndarray<int>* _IntArray;
-typedef struct _ndarray<long int>* _LongIntArray;*/
